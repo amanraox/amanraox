f64a1fd192aa56b64b1bca522af117aa6d8da7f7
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-05-17 20:22:11 +0530

    Update shell.js

--- js/shell.js
+++ js/shell.js
@@ -12,11 +12,7 @@ const welcome = `<div class="nomobile">
 <span data-color="yellow">So finally you found this corner of the internet</span>
 
 Welcome!
-I'm <span data-color="red"><a href="https://linkedin.com/in/amanraox">Aman Umrao</a></span>, a self-taught <span data-color="yellow">Web Developer</span> and hobbyist <span data-color="yellow">Competitive Programmer</span> from <span data-color="lime">India</span>🇮🇳️.
-My passion lies in leveraging coding to solve real-world problems, whether it's developing efficient algorithms or creating user-friendly websites.
-When I'm not working on personal projects, you can find me solving problems on Codeforces or exploring the latest trends in the tech world.
-As a Linux enthusiast and advocate for open-source software, I'm dedicated to contributing to innovative projects that drive meaningful change.
-Feel free to explore my <span data-color="cyan"><a href="https://github.com/amanraox">GitHub</a></span> to see my latest work, and don't hesitate to reach out if you'd like to collaborate or <span data-color="cyan"><a href="https://linkedin.com/in/amanraox">connect</a></span>!<br>
+I'm <span data-color="red"><a href="https://linkedin.com/in/amanraox">Aman Umrao</a></span>, a self-taught <span data-color="yellow">Web Developer</span> and hobbyist <span data-color="yellow">Competitive Programmer</span> from <span data-color="lime">India</span>.🇮🇳️
 <b>Play around here..<b>Type <span data-color="yellow">help</span><br> `;
 const sleep = (m) => new Promise((r) => setTimeout(r, m));
 const terminal = document.querySelector("pre");
