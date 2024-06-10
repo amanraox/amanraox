a246241b0107e6189ee94d35294c0c3aa658b240
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-06-10 18:12:56 +0530

    Update help.js

--- js/cmds/help.js
+++ js/cmds/help.js
@@ -11,9 +11,9 @@ Here Is A List Of Commands:
     <span data-color="#00ff99">socials [platform?]</span> <span data-color="grey">-</span> <span data-color="white">Contact Me Here!</span>
 <span data-color="grey">--------------------------------------------</span>
 Extra Commands For Fun:
-    <span data-color="#00ff99">clear              </span> <span data-color="grey">-</span> <span data-color="white">Clears The Terminal.</span>
+    <span data-color="#00ff99">clear              </span> <span data-color="grey">-</span> <span data-color="white">Clears Terminal.</span>
     <span data-color="#00ff99">whoami             </span> <span data-color="grey">-</span> <span data-color="white">Aap kon?</span>
-    <span data-color="#00ff99">history            </span> <span data-color="grey">-</span> <span data-color="white">Prints Your Command History.</span>
+    <span data-color="#00ff99">history            </span> <span data-color="grey">-</span> <span data-color="white">cout Command History.</span>
     <span data-color="#00ff99">hostname           </span> <span data-color="grey">-</span> <span data-color="white">What Is This?</span>
     <span data-color="#00ff99">echo [text]        </span> <span data-color="grey">-</span> <span data-color="white">Prints [text].</span>
     <span data-color="#00ff99">eval [code]        </span> <span data-color="grey">-</span> <span data-color="white">Eval [code] As JS.</span>
