29571300346ceaa9b96d137f8f9af9caf46ef690
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-08-29 01:06:00 +0530

    Delete js directory

--- js/cmds/source.js
+++
@@ -1,4 +0,0 @@
-export default function(output) {
-    output.innerHTML += '<span data-color="orange">Opening Github...</span>';
-    window.open('https://github.com/amanraox');
-}
--- js/cmds/whoami.js
+++
@@ -1,3 +0,0 @@
-export default function(output) {
-    output.innerHTML += "IDK but you seems nice person. we can be friends :)";
-}
